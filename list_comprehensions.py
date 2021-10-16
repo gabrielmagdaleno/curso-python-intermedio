ec4da882b5e71a4c7505fe8acbd645458a2b98f9
Gabriel Magdaleno <gabrielmagdalenoaular@gmail.com>
2021-10-16 19:24:42 +0200

    Agregando las primeras clases y los primeros ejercicios del curso intermedio de python

---
+++ list_comprehensions.py
@@ -0,0 +1,34 @@
+# #El cuadrado de los primeros 100 numeros naturales 
+# def run():
+#     squares = []
+#     for i in range(1, 101):
+#         squares.append(i**2)
+        
+#         print(squares)
+    
+    
+# if __name__ == '__main__':
+#     run()
+    
+#El cuadrado de los primeros 100 numeros naturales menos los que no son divisibles entre 3
+def run():
+    # squares = []
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         squares.append(i**2)
+    
+    # squares = [i**2 for i in range(1, 101) if i % 3 != 0]
+    
+    
+    squares = [i for i in range(1, 100000) if i % 3 == 0 and i % 4 == 0]
+        
+    print(squares)
+    
+    
+if __name__ == '__main__':
+    run()
+    
+
+# #Una forma eficiente de reasolver el reto
+# squares = [ i for i in range(1, 100000) if i % 36 == 0]
+# print(squares)
