053438f9398932c596f9d1a36a0c27d304a1332f
Gabriel Magdaleno <gabrielmagdalenoaular@gmail.com>
2021-10-23 01:37:34 +0200

    Anidando listas y diccionarios

--- list_and_dicts.py
+++ list_and_dicts.py
@@ -1,6 +1,7 @@
 def run():
     my_list = [1, "Hello", True, 4.5]
     my_dict = {"firstname": "Gabriel", "lastname": "Magdaleno"}
+    
 
     super_list = [
         {"firstname": "Gabriel", "lastname": "Magdaleno"},
@@ -10,14 +11,17 @@ def run():
         {"firstname": "José", "lastname": "García"},
     ]
     
-    super_dict = {
-        "natural_nums":[1,2,3,4,5],
-        "integer_nums":[-1,-2,0, 1, 2],
-        "floating_nums":[1.1, 4.5, 6.43]
-    }
+    # super_dict = {
+    #     "natural_nums":[1,2,3,4,5],
+    #     "integer_nums":[-1,-2,0, 1, 2],
+    #     "floating_nums":[1.1, 4.5, 6.43]
+    # }
     
-    for key,value in super_dict.items():
-        print(key, "-", value)
+    # # for key,value in super_dict.items():
+    # #     print(key, "-", value)
+    
+    for item in  super_list:
+        print(item ["firstname"], ",", item ["lastname"])
 
 
 if __name__ == '__main__':
