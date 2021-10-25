360c277a7d46b8b68176bcdd043fc2b8156a34e2
Gabriel Magdaleno <gabrielmagdalenoaular@gmail.com>
2021-10-25 19:39:36 +0200

    Agragando inicio del módulo de errores

--- filtrandoDatos.py
+++ filtrandoDatos.py
@@ -73,34 +73,45 @@ DATA = [
 
 def run():
 
-# ** Usando list Comprehensions
+#* Usando list Comprehensions
 
     # all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    
     # all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
     
-    # #for worker in all_python_devs:
+    # for worker in all_python_devs:
+    #     print(worker)
     
     # for worker in all_Platzi_workers:
     #     print(worker)
     
-# ** High order funtions
+#* High order funtions
 
     # adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    
     # adults = list(map(lambda worker: worker["name"], adults))
-    # old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70} , DATA))
-
-    # for worker in  old_people:
-    #         print(worker)
     
-# ** Reto Clase 13
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
 
-    # pythonDevs = list(filter(lambda worker: worker["language"], DATA))
-    # # pythonDevs = list(map(lambda worker: worker["language"], pythonDevs))
-    # pythonDevs = list(map(lambda worker: worker["organization"], pythonDevs))
-    pythonDevs = list(map(lambda worker: worker | {"languageOrg": worker["lenguage"], "org": worker ["organization"]}, DATA))
+    # for worker in  adults:
+    #     print(worker)
     
-    for worker in  pythonDevs:
+
+    for worker in  old_people:
         print(worker)
+    
+#* Reto Clase 13
+
+    # pythonDevs = list(filter(lambda worker: worker["name"], DATA))
+    
+    # pythonDevs = list(map(lambda worker: worker["language"], pythonDevs))
+    
+    # pythonDevs = list(map(lambda worker: worker["organization"], pythonDevs))
+    
+    # pythonDevs = list(map(lambda worker: worker | {"languageOrg": worker["lenguage"], "org": worker ["organization"]}, DATA))
+    
+    # for worker in  pythonDevs:
+    #     print(worker)
 
 if __name__ == '__main__':
     run()
